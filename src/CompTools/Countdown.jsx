0ea3b98e63df53d0ebc9db4a478f9da9b8890be2
Alexis Serna Aguilera <115396600+iiiamalex@users.noreply.github.com>
2025-05-20 01:14:23 -0500

    ToolComp for Home Page

---
+++ src/CompTools/Countdown.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const Countdown = ({ targetDate }) => {
+    const calculateTimeLeft = () => {
+        const difference = +new Date(targetDate) - +new Date();
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60),
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    });
+
+    return (
+        <div className="countdown-grid">
+            <div><span>{timeLeft.days || 0}</span><p>Days</p></div>
+            <div><span>{timeLeft.hours || 0}</span><p>Hours</p></div>
+            <div><span>{timeLeft.minutes || 0}</span><p>Minutes</p></div>
+            <div><span>{timeLeft.seconds || 0}</span><p>Seconds</p></div>
+        </div>
+    );
+};
+
+export default Countdown;
