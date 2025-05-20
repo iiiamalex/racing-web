fbd7a5eb4c08cc7f3374a125d854cdd66c1a3a73
Alexis Serna Aguilera <115396600+iiiamalex@users.noreply.github.com>
2025-05-20 00:52:23 -0500

    Outline Completed

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Navbar from "./StaticComps/Navbar";
+import HomePage from "./Pages/HomePage";
+import ContactPage from "./Pages/ContactPage";
+import GalleryPage from "./Pages/GalleryPage";
+import SponsorsPage from "./Pages/SponsorsPage";
+import Footer from "./StaticComps/Footer";
+
+function App() {
+
+    return (
+        <>
+            <div className="global-video-wrapper">
+                <video autoPlay muted loop playsInline className="global-bg-video">
+                    <source src="/Assets/Background.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <div className="global-video-overlay"></div>
+            </div>
+
+            <div className="app-content">
+
+                <Navbar />
+
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="gallery" element={<GalleryPage />} />
+                    <Route path="sponsors" element={<SponsorsPage />} />
+                    <Route path="contact" element={<ContactPage />} />
+                </Routes>
+
+                <Footer />
+
+            </div>
+        </>
+    );
+}
+
+export default App
